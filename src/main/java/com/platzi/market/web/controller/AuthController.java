b963081bd7409e5b3a68b61a3f7189f62efa5121
SergioCardoso97 <sergio.cardhdez97@gmail.com>
2023-03-13 14:00:17 -0600

    Primer Commit

---
+++ src/main/java/com/platzi/market/web/controller/AuthController.java
@@ -0,0 +1,41 @@
+package com.platzi.market.web.controller;
+
+import com.platzi.market.domain.dto.AutenticationRequest;
+import com.platzi.market.domain.dto.AutenticationResponse;
+import com.platzi.market.domain.service.PlatziUserDetailsService;
+import com.platzi.market.web.security.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private PlatziUserDetailsService platziUserDetailsService;
+    @Autowired
+    private JWTUtil jwtUtil;
+    @PostMapping("/authenticate")
+    public ResponseEntity<AutenticationResponse> createToken(@RequestBody AutenticationRequest request){
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(),request.getPassword()));
+            UserDetails userDetails = platziUserDetailsService.loadUserByUsername(request.getUsername());
+            String jwt = jwtUtil.generateToken(userDetails);
+            return new ResponseEntity<>(new AutenticationResponse(jwt), HttpStatus.OK);
+        }
+        catch (BadCredentialsException e){
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+    }
+}
